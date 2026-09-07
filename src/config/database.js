5b79f532c47bebbc74892baed39271cd501b8153
Govindraj Ingle <govindrajingle@gmail.com>
2026-09-08 00:00:56 +0530

    db map to railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
 });
 
 const testDatabaseConnection = async () => {
